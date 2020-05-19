792de44b681c6195ece4257379a209ba2765dd56
Razak, Zaheir <zrazak3@gatech.edu>
2020-05-19 16:54:34 -0400

    Add project files.

---
+++ FutureValue/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FutureValue.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Mary";
+            ViewBag.FV = 99999.99;
+            return View();
+        }
+    }
+}
